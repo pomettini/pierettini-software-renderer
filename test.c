c71360078a7634aa3d31221edd69a55ae8fd82cb
Piera Falcone <pierafalcone01@gmail.com>
2019-01-08 15:16:01 +0100

    added first test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "aiv_unit_test.h"
+#include "math.h"
+
+TEST(screen_space_to_pixel_conversion)
+{
+    point2_t result = screen_space_to_pixel(0.5, 0.5, 400, 400);
+
+    ASSERT_THAT(result.x == 200);
+    ASSERT_THAT(result.y == 200);
+
+}
+
+int main(int argc, char **argv)
+{
+    RUN_TEST(run_test_screen_space_to_pixel_conversion);
+
+    PRINT_TEST_RESULTS();
+    return 0;
+}
