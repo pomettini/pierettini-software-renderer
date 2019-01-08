61f009b2301ef0354ef4004c0ed4f4c6281b91bb
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2019-01-08 14:41:11 +0100

    Added first implementation

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+typedef struct point2
+{
+    float x;
+    float y;
+} point2_t;
+
+point2_t screen_space_to_pixel_space(float x, float y, int width, int height)
+{
+    point2_t point;
+
+    point.x = 0.0;
+    point.y = 0.0;
+
+    return point;
+}
+
 int main()
 {
     return 0;
