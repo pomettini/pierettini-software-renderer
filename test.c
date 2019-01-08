e86b7886c5514b5ae8622912f5e7b137bb03b5ab
Piera Falcone <pierafalcone01@gmail.com>
2019-01-08 16:39:58 +0100

    guess what? Added some new tests

--- test.c
+++ test.c
@@ -16,11 +16,29 @@ TEST(screen_space_to_pixel_conversion_negative)
     ASSERT_THAT(result.x == 100);
     ASSERT_THAT(result.y == 100);
 }
+TEST(screen_space_to_pixel_overflow)
+{
+    point2_t result = screen_space_to_pixel(2, 2, 400, 400);
 
+    ASSERT_THAT(result.x == 400);
+    ASSERT_THAT(result.y == 400);
+
+}
+
+TEST(screen_space_to_pixel_overflow_negative)
+{
+    point2_t result = screen_space_to_pixel(-2, -2, 400, 400);
+
+    ASSERT_THAT(result.x == 0);
+    ASSERT_THAT(result.y == 0);
+
+}
 int main(int argc, char **argv)
 {
     RUN_TEST(screen_space_to_pixel_conversion);
     RUN_TEST(screen_space_to_pixel_conversion_negative);
+    RUN_TEST(screen_space_to_pixel_overflow);
+    RUN_TEST(screen_space_to_pixel_overflow_negative);
 
     PRINT_TEST_RESULTS();
     return 0;
